62fac5bd7e6c3d427b3e6b708bda785542e6caf4
sanketwalke <sanketwalke07@gmail.com>
2022-04-19 23:01:55 +0530

    customer-bs

---
+++ customer-bs/src/main/java/com/shopify/business/customerbs/CustomerBsApplication.java
@@ -0,0 +1,13 @@
+package com.shopify.business.customerbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerBsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerBsApplication.class, args);
+	}
+
+}
---
+++ customer-bs/src/test/java/com/shopify/business/customerbs/CustomerBsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopify.business.customerbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerBsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
